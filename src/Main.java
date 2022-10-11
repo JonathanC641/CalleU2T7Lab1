e3fc4ae9256fdae7762014124efce61dd1a34add
1E10 Laptop <sample@bths.edu>
2022-10-11 10:57:16 -0400

    DOPE CODE

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scan.nextLine();
+        int lenWord = word.length();
+        System.out.print(lenWord);
+        System.out.print("WASSUP");
+
+    }
+}
